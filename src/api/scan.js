238a1f37b90b3b338822124b2eb66c52c85744ab
Guilherme Penso <guilhermepenso@gmail.com>
2024-02-23 05:56:37 -0300

    updating tables

--- src/api/scan.js
+++ src/api/scan.js
@@ -1,16 +1,19 @@
 import axios from 'axios';
-import { API_ENDPOINT, ACCESS_TOKEN } from '@env';
+import { API_ENDPOINT } from '@env';
 
 const apiEndpoint = API_ENDPOINT;
-const accessToken = ACCESS_TOKEN;
+// const accessToken = ACCESS_TOKEN;
 
 export const scanProducts = async () => {
     try {
+        /*
         const response = await axios.get(`${apiEndpoint}/products`, {
             headers: {
                 Authorization: `Bearer ${accessToken}`
             }
         });
+        */
+        const response = await axios.get(`${apiEndpoint}/products`);
         console.log('Products:', response.data.products);
         return response.data.products
     } catch (error) {
